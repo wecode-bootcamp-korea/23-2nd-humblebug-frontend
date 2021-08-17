274b0e5afcf9f2cc7186adecf9bffde0dad0631b
mateo <materokatti@gmail.com>
2021-08-17 16:59:06 +0900

    ADD: 초기 세팅 완료

---
+++ src/src/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const Footer = () => {
+  return <div></div>;
+};
+export default Footer;
---
+++ src/src/components/Nav/Nav.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const Nav = () => {
+  return <div></div>;
+};
+export default Nav;
