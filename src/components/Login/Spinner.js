049c2ac92d8725a24c165bee164c20556b6fd6fa
Lown Kim <wideplate3@gmail.com>
2021-08-19 18:24:39 +0900

    Add: 로그인페이지 완성

---
+++ src/components/Login/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { KAKAO_AUTH_URL } from '../../config';
+
+const Spinner = props => {
+  let code = new URL(window.location.href).searchParams.get('code');
+  console.log('code:', code);
+  fetch('http://kauth.kakao.com/oauth/token')
+    .then(res => res.json())
+    .then(res => console.log('Success:', JSON.stringify(res)));
+  return <div>기다리세요</div>;
+};
+export default Spinner;
--- src/config.js
+++ src/config.js
@@ -1,7 +1,14 @@
-const BASE_URL = 'http://10.58.0.158:8000';
+const BASE_URL = 'http://10.58.3.43:8000';
+
 export const API = {
   DETAIL_GET: `${BASE_URL}/project/4`, //상세페이지 데이터
   DETAIL_OPTION: `${BASE_URL}/project/4/option`, //상세페이지 옵션 데이터
   DETAIL_COMMENT_GET: `${BASE_URL}/project/4/comments`, //상세페이지 댓글 GET
   DETAIL_COMMENT_POST: `${BASE_URL}/project/4/comment`, //상세페이지 댓글 POST
+  SIGNIN: `${BASE_URL}/users/signin`,
 };
+// const BASE_URL = 'http://3.144.112.76:8000';
+// const BASE_URL = 'http://10.58.3.43:8000';
+const CLIENT_ID = 'd4e1f9d408876dd8623f2142a65475d7';
+const REDIRECT_URI = 'http://localhost:3000/users/signin';
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
