9d1361d110df7530c56ef9f302a99c9907d60fc1
Lown Kim <wideplate3@gmail.com>
2021-08-24 20:28:42 +0900

    Modify: Footer레이아웃 수정

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,70 +6,73 @@ const Footer = () => {
     <footer>
       {/* 푸터 윗부분 */}
       <FooterUpper>
-        <ContentsWrapper>
-          <Mass>
-            <FirstOne>텀블벅</FirstOne>
-            <ContentEach>공지사항</ContentEach>
-            <ContentEach>채용</ContentEach>
-            <ContentEach>제휴∙협력</ContentEach>
-          </Mass>
-          <Mass>
-            <FirstOne>이용안내</FirstOne>
-            <ContentEach>헬프 센터</ContentEach>
-            <ContentEach>창작자 가이드</ContentEach>
-            <ContentEach>프로젝트 심사 기준</ContentEach>
-            <ContentEach>수수료 정책</ContentEach>
-          </Mass>
-          <Mass>
-            <FirstOne>정책</FirstOne>
-            <ContentEach>이용약관</ContentEach>
-            <ContentEach>개인정보 처리방침</ContentEach>
-          </Mass>
-          <Mass>
-            <FirstOne>App</FirstOne>
-            <ContentEach>
-              <AndroidBtn>안드로이드</AndroidBtn>
-            </ContentEach>
-            <li>
-              <IosBtn>iOS</IosBtn>
-            </li>
-          </Mass>
-        </ContentsWrapper>
-        <div>
-          <FirstOne>고객지원</FirstOne>
-          <ContentEach>평일 10:00 ~ 17:00 (12:00 ~ 14:00 제외)</ContentEach>
-          <Inquire>험블벅에 문의</Inquire>
-        </div>
+        <SetWidth>
+          <ContentsWrapper>
+            <Mass>
+              <FirstOne>텀블벅</FirstOne>
+              <ContentEach>공지사항</ContentEach>
+              <ContentEach>채용</ContentEach>
+              <ContentEach>제휴∙협력</ContentEach>
+            </Mass>
+            <Mass>
+              <FirstOne>이용안내</FirstOne>
+              <ContentEach>헬프 센터</ContentEach>
+              <ContentEach>창작자 가이드</ContentEach>
+              <ContentEach>프로젝트 심사 기준</ContentEach>
+              <ContentEach>수수료 정책</ContentEach>
+            </Mass>
+            <Mass>
+              <FirstOne>정책</FirstOne>
+              <ContentEach>이용약관</ContentEach>
+              <ContentEach>개인정보 처리방침</ContentEach>
+            </Mass>
+            <Mass>
+              <FirstOne>App</FirstOne>
+              <ContentEach>
+                <AndroidBtn>안드로이드</AndroidBtn>
+              </ContentEach>
+              <li>
+                <IosBtn>iOS</IosBtn>
+              </li>
+            </Mass>
+          </ContentsWrapper>
+          <MarginLeft>
+            <FirstOne>고객지원</FirstOne>
+            <ContentEach>평일 10:00 ~ 17:00 (12:00 ~ 14:00 제외)</ContentEach>
+            <Inquire>험블벅에 문의</Inquire>
+          </MarginLeft>
+        </SetWidth>
       </FooterUpper>
       {/* 푸터 아랫부분 */}
       <FooterBottom>
-        <div>
-          <BottomContents>
-            <BlackColor>회사명</BlackColor> 험블벅(주)
-            <BlackColor> 주소</BlackColor> 서울시 강남구 삼성동 테헤란로427,
-            위워크타워 10층
-            <BlackColor> 대표</BlackColor> 험블벅
-            <BlackColor> 사업자등록번호</BlackColor> 2021-08-17
-            <br />
-            <BlackColor> 통신판매업 신고번호</BlackColor> 2021-08-17
-            <BlackColor> 대표번호</BlackColor> 2021-08-17
-            <br />© 2021 Humblbug Inc.
-          </BottomContents>
-        </div>
-        <IconsWrapper>
-          <Images alt="talk" src="/images/talk.png" />
-          <Images alt="facebook" src="/images/facebook.png" />
-          <Images alt="twitter" src="/images/twitter.png" />
-          <Images alt="instagram" src="/images/instagram.png" />
-          <Images alt="naver" src="/images/naver.png" />
-        </IconsWrapper>
+        <SetWidth>
+          <div>
+            <BottomContents>
+              <BlackColor>회사명</BlackColor> 험블벅(주)
+              <BlackColor> 주소</BlackColor> 서울시 강남구 삼성동 테헤란로427,
+              위워크타워 10층
+              <BlackColor> 대표</BlackColor> 험블벅
+              <BlackColor> 사업자등록번호</BlackColor> 2021-08-17
+              <br />
+              <BlackColor> 통신판매업 신고번호</BlackColor> 2021-08-17
+              <BlackColor> 대표번호</BlackColor> 2021-08-17
+              <br />© 2021 Humblbug Inc.
+            </BottomContents>
+          </div>
+          <IconsWrapper>
+            <Images alt="talk" src="/images/talk.png" />
+            <Images alt="facebook" src="/images/facebook.png" />
+            <Images alt="twitter" src="/images/twitter.png" />
+            <Images alt="instagram" src="/images/instagram.png" />
+            <Images alt="naver" src="/images/naver.png" />
+          </IconsWrapper>
+        </SetWidth>
       </FooterBottom>
     </footer>
   );
 };
 
 const FooterUpper = styled.div`
-  display: flex;
   padding: 30px;
   border-top: 1px solid #e6e6e6;
   border-bottom: 1px solid #e6e6e6;
@@ -80,8 +83,18 @@ const ContentsWrapper = styled.div`
   display: flex;
 `;
 
+const SetWidth = styled.div`
+  margin: 0 auto;
+  display: flex;
+  width: 1070px;
+`;
+
 const Mass = styled.ul`
   margin-right: 50px;
+`;
+
+const MarginLeft = styled.div`
+  margin-left: auto;
 `;
 
 const FirstOne = styled.li`
@@ -143,6 +156,7 @@ const BlackColor = styled.span`
 `;
 
 const IconsWrapper = styled.div`
+  margin-left: auto;
   color: black;
 `;
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import Routes from './Routes';
-import theme from './styles/theme';
+import theme from './styles/Theme';
 
 ReactDOM.render(
   <>
