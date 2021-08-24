8b38fc979f0b88454556d1326a45be27e920ea0e
cocacollllla <cocacollllla@gmail.com>
2021-08-25 00:42:08 +0900

    Add : Filter 페이지 데이터 통신 완료

---
+++ src/pages/Filter/filterCategory.js
@@ -0,0 +1,9 @@
+const FILTER_CATEGORY = [
+  { id: 1, list: '커머스' },
+  { id: 2, list: '예약' },
+  { id: 3, list: '리뷰' },
+  { id: 4, list: '커뮤니티' },
+  { id: 5, list: '그 외' },
+];
+
+export default FILTER_CATEGORY;
